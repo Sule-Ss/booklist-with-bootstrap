0ab5c75194fb72774b809d1cabcd32a98257ed9d
Sule <aybilenden@gmail.com>
2022-05-05 19:06:14 +0300

    fixed

--- style.js
+++ style.js
@@ -8,25 +8,23 @@ let input = document.querySelectorAll("input");
 
 title.focus();
 
-const createTask = (title, author, date) => {
-    
-  return `
-        <tr>
-          <td class="text-capitalize">${title}</td>
-          <td class="text-capitalize">${author}</td>
-          <td class="text-capitalize">${date}</td>
-          <td class="text-capitalize btn del-btn btn-outline-info rounded-circle mt-1">x</td>
-        </tr>`;
-};
+let obj = [];
+
+obj = JSON.parse(localStorage.getItem("taskList")) || [];
+console.log(obj);
+
 button.addEventListener("click", (e) => {
   e.preventDefault();
   if (input[0].value !== "" && input[1].value !== "" && input[2].value !== "") {
-    bookList.innerHTML += createTask(title.value,author.value, date.value);
 
-    objCreate();
-    
+    obj.push({ title: title.value, author: author.value, date: date.value });
+
+    localStorage.setItem("taskList", JSON.stringify(obj));
+
+    bookList.innerHTML = "";
+    createTask();
+    console.log(obj);
     form.reset();
-
   } else {
     alert("please fill in the empty boxes");
     form.reset();
@@ -34,24 +32,14 @@ button.addEventListener("click", (e) => {
   }
 });
 
-let obj = [];
-function objCreate(){
-  // eğer önceden kaydımız varsa onu çekiyoruz yoksa boş bir tane oluşturuyoruz:
-  obj = JSON.parse(localStorage.getItem("library"))||[];
-  //OBJ ye yeni veri atama:
-  obj = [{title:title.value, author: author.value, date : date.value}];
-
-  // local storageye kaydetme
-  localStorage.setItem("library", JSON.stringify(obj))
-}
-function create2(){
-
-  // localstorageden geri çekme
-  obj = JSON.parse(localStorage.getItem("library"));
-  console.log(obj);
-  obj.forEach(item => {
-    // console.log(item.title);
-    bookList.innerHTML += createTask(item.title, item.author, item.date);
+function createTask() {
+  obj?.map(({ title, author, date }) => {
+    return (bookList.innerHTML += `<tr>
+      <td class="text-capitalize">${title}</td>
+      <td class="text-capitalize">${author}</td>
+      <td class="text-capitalize">${date}</td>
+      <td class="text-capitalize btn del-btn btn-outline-info rounded-circle mt-1">x</td>
+    </tr>`)
   });
 }
 
@@ -74,4 +62,4 @@ author.addEventListener("keydown", (e) => {
   }
 });
 
-document.addEventListener("DOMContentLoaded", create2);
+document.addEventListener("DOMContentLoaded", createTask);
